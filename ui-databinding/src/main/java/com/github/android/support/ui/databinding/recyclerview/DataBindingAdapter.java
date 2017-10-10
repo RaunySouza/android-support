f41cc4ee8cbdc688548156679200bfdc139abed5
raunysouza <rauny.souza@gmail.com>
2017-10-10 11:38:30 -0300

    Creates get method for context property Adds hook method to supply an item id

--- ui-databinding/src/main/java/com/github/android/support/ui/databinding/recyclerview/DataBindingAdapter.java
+++ ui-databinding/src/main/java/com/github/android/support/ui/databinding/recyclerview/DataBindingAdapter.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,13 +19,17 @@ import java.util.List;
 public abstract class DataBindingAdapter<T, B extends ViewDataBinding> extends RecyclerView.Adapter<DataBindingViewHolder<B>> {
 
     private Context mContext;
-    private List<T> mItems;
+    private List<T> mItems = new ArrayList<>();
     @LayoutRes
     private int mLayoutRes;
 
     public DataBindingAdapter(Context context, List<T> items, @LayoutRes int layoutRes) {
+        this(context, layoutRes);
+        mItems = items;
+    }
+
+    public DataBindingAdapter(Context context, @LayoutRes int layoutRes) {
         this.mContext = context;
-        this.mItems = items;
         this.mLayoutRes = layoutRes;
     }
 
@@ -43,19 +48,28 @@ public abstract class DataBindingAdapter<T, B extends ViewDataBinding> extends R
     protected abstract void onAttachBinding(B binding, T item);
 
     @Override
+    public long getItemId(int position) {
+        return getItemIdSupplier(mItems.get(position));
+    }
+
+    protected long getItemIdSupplier(T item) {
+        return RecyclerView.NO_ID;
+    }
+
+    @Override
     public int getItemCount() {
-        if (mItems == null) {
-            return 0;
-        }
         return mItems.size();
     }
 
-    @Nullable
+    public boolean isEmpty() {
+        return mItems.isEmpty();
+    }
+
     public List<T> getItems() {
         return mItems;
     }
 
-    public void setItems(List<T> items) {
+    public void setItems(@NonNull List<T> items) {
         mItems = items;
         notifyDataSetChanged();
     }
@@ -82,5 +96,10 @@ public abstract class DataBindingAdapter<T, B extends ViewDataBinding> extends R
             mItems.remove(index);
             notifyItemRemoved(index);
         }
+    }
+
+    @NonNull
+    protected Context getContext() {
+        return mContext;
     }
 }
